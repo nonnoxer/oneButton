f337b691d866b7e0ab4a674e2c604383b71c2e7b
Natanael Tan <nonnoxer@gmail.com>
2019-04-18 17:47:36 +0800

    stashed

--- classes.py
+++ classes.py
@@ -4,43 +4,11 @@ pygame.init()
 pygame.font.init()
 import variables
 
-class Button(pygame.sprite.Sprite):
-    def __init__(self, x, y, size):
+class Player(pygame.sprite.Sprite):
+    def __init__(self):
         pygame.sprite.Sprite.__init__(self)
-        self.img = pygame.image.load('assets/Button.png')
-        self.rect = self.img.get_rect()
-        self.rect.x = x
-        self.rect.y = y
-        self.mask = pygame.mask.from_surface(self.img)
-        self.surface = pygame.Surface((size, size), pygame.SRCALPHA, 32)
-        self.surface = self.surface.convert_alpha()
+        self.img = pygame.image.load('assets/Jumper.png')
     def draw(self):
-        self.surface.blit(self.img, (0, 0))
-        return self.surface
-
-class Sentry(pygame.sprite.Sprite):
-    def __init__(self, x, y):
-        pygame.sprite.Sprite.__init__(self)
-        self.original = pygame.image.load('assets/Sentry.png')
-        self.img = self.original
-        self.orirect = self.img.get_rect()
-        self.orirect.centerx = x
-        self.orirect.centery = y
-        self.rect = self.img.get_rect()
-        self.rect.centerx = self.orirect.centerx
-        self.rect.centery = self.orirect.centery
-        self.mask = pygame.mask.from_surface(self.img)
-        self.a = 1
-    def draw(self):
-        self.surface = pygame.Surface((self.rect.bottom, self.rect.right), pygame.SRCALPHA, 32)
-        self.surface = self.surface.convert_alpha()
-        self.surface.blit(self.img, (0, 0))
-        return self.surface
+        pass
     def update(self):
-        self.img = pygame.transform.rotate(self.original, self.a) #dont even know what i did it just works lol
-        self.rect = self.img.get_rect()
-        self.rect.centerx = self.orirect.centerx
-        self.rect.centery = self.orirect.centery
-        self.a = self.a + 1
-        if self.a == 360:
-            self.a = 0
+        pass
--- main.py
+++ main.py
@@ -5,25 +5,18 @@ pygame.font.init()
 import classes
 import variables
 
-button = classes.Button(304, 304, 32)
-sentries = []
-sentries.append(classes.Sentry(100, 100))
+
 
 class Main(object):
     def __init__(self):
         while not variables.done:
             for event in pygame.event.get():
-                if event.type == pygame.MOUSEBUTTONDOWN:
-                    sentries.append(classes.Sentry(pygame.mouse.get_pos()[0], pygame.mouse.get_pos()[1]))
                 if event.type == pygame.QUIT:
                     variables.done = True
 
             variables.screen.fill(variables.BGCOLOUR)
 
-            variables.screen.blit(button.draw(), (button.rect.x, button.rect.y))
-            for i in sentries:
-                variables.screen.blit(i.draw(), (i.rect.x, i.rect.y))
-                i.update()
+            
 
             pygame.display.flip()
             variables.clock.tick(variables.FRAMERATE)
